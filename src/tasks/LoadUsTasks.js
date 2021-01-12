b2f8a95f976f46e4951283801e19f8d1c03130a7
Jomar HA Nguyen <jomarhan.ng@gmail.com>
2021-01-12 15:47:26 -0500

    done table us

--- src/tasks/LoadUsTasks.js
+++ src/tasks/LoadUsTasks.js
@@ -1,6 +1,22 @@
 class LoadUsTasks {
     setUpdate = null
 
+    loadListTable = (setListRegion) => {
+        fetch('https://disease.sh/v3/covid-19/states')
+            .then(res => res.json())
+            .then(data => {
+                const listSorted = this.#sortStatesList(data);
+                setListRegion(listSorted)
+            })
+    }
+    #sortStatesList = (mapUsStates) => {
+        const statesList = [...mapUsStates]
+        statesList.sort((a, b) => {
+            return a.cases > b.cases ? -1 : 1
+        })
+        return statesList
+    }
+
     loadUsCard = (setUpdate) => {
         this.setUpdate = setUpdate
         fetch('https://disease.sh/v3/covid-19/countries/us')
