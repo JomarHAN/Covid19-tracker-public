2ab45222bbf2e881aaef060384f9849d206c59bc
Jomar HA Nguyen <jomarhan.ng@gmail.com>
2021-01-13 13:50:06 -0500

    done on us map

--- src/components/UsMap/UsMap.jsx
+++ src/components/UsMap/UsMap.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { MapContainer } from "react-leaflet";
 import { useSelector } from "react-redux";
 import { selectCasesType } from "../../features/casesTypeSlice";
@@ -7,22 +7,68 @@ import CasesMap from "../GeoMapType/CasesMap";
 import DeathsMap from "../GeoMapType/DeathsMap";
 import RecoveredMap from "../GeoMapType/RecoveredMap";
 import "./UsMap.css";
+import numeral from "numeral";
 
 function UsMap({ usStates }) {
   const casesType = useSelector(selectCasesType);
   const usCenter = useSelector(selectUsCenter);
   const usZoom = useSelector(selectUsZoom);
+  const [regionHover, setRegionHover] = useState({
+    regionName: "USA",
+    regionCases: null,
+    regionDeaths: null,
+    regionRecovered: null,
+  });
+
   return (
     <MapContainer style={{ height: "100%" }} center={usCenter} zoom={usZoom}>
       {usStates?.map((usState) => {
         if (casesType === "cases") {
-          return <CasesMap region={usState} />;
+          return <CasesMap region={usState} setHover={setRegionHover} />;
         } else if (casesType === "deaths") {
-          return <DeathsMap region={usState} />;
+          return <DeathsMap region={usState} setHover={setRegionHover} />;
         } else {
-          return <RecoveredMap region={usState} />;
+          return <RecoveredMap region={usState} setHover={setRegionHover} />;
         }
       })}
+      <div
+        style={{
+          position: "absolute",
+          right: "10px",
+          top: "10px",
+          border: "1px solid lightgray",
+          padding: "5px",
+          backgroundColor: "lightgray",
+          borderRadius: "5px",
+          zIndex: 999,
+        }}
+      >
+        {regionHover.regionName === "USA" ? (
+          <h4>
+            Scope on: <strong>{regionHover.regionName}</strong>
+          </h4>
+        ) : (
+          <>
+            <h4>
+              Scope on: <strong>{regionHover.regionName}</strong>
+            </h4>
+            <p>
+              Cases:{" "}
+              <strong>{numeral(regionHover.regionCases).format("0,0")}</strong>
+            </p>
+            <p>
+              Deaths:{" "}
+              <strong>{numeral(regionHover.regionDeaths).format("0,0")}</strong>
+            </p>
+            <p>
+              Recovered:{" "}
+              <strong>
+                {numeral(regionHover.regionRecovered).format("0,0")}
+              </strong>
+            </p>
+          </>
+        )}
+      </div>
     </MapContainer>
   );
 }
--- src/tasks/LoadUsTasks.js
+++ src/tasks/LoadUsTasks.js
@@ -1,11 +1,40 @@
 import { features } from '../dataLatLng/us-states.json'
+import { legendStates } from '../entities/LegendItems'
 
 class LoadUsTasks {
     setUpdate = null
     setUsStates = null
+    mapUsStates = features
 
     loadUsMap = (setUsStates) => {
-        setUsStates(features)
+        this.setUsStates = setUsStates
+        fetch('https://disease.sh/v3/covid-19/states')
+            .then(res => res.json())
+            .then(data => this.#processDataColor(data))
+    }
+
+    #processDataColor = (data) => {
+        for (let i = 0; i < this.mapUsStates.length; i++) {
+            const eachState = this.mapUsStates[i];
+            const stateCovid = data.find(stateCovid => stateCovid.state === eachState.properties.NAME)
+            if (stateCovid != null) {
+                eachState.properties = stateCovid
+            }
+            this.#setStateColor(eachState)
+        }
+        this.setUsStates(this.mapUsStates)
+    }
+
+    #setStateColor = (eachState) => {
+        const itemCases = legendStates[0].legends.find(item => item.isFor(eachState.properties.casesPerOneMillion))
+        const itemDeaths = legendStates[1].legends.find(item => item.isFor(eachState.properties.deathsPerOneMillion))
+        const itemRecovered = legendStates[2].legends.find(item => item.isFor(eachState.properties.recovered))
+
+        if (itemCases !== null || itemDeaths !== null || itemRecovered !== null) {
+            eachState.properties.colorCases = itemCases.color
+            eachState.properties.colorDeaths = itemDeaths.color
+            eachState.properties.colorRecovered = itemRecovered.color
+        }
     }
 
     loadListTable = (setListRegion) => {
